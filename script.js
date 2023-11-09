44b1aa8422953037f2f9a87fe448977e6af49eac
dnlzqn <55982609+dnlzqn@users.noreply.github.com>
2023-11-09 17:56:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-g=10,o=0,c=0,setup=o=>{createCanvas(W=windowWidth,H=windowHeight),noStroke()},draw=n=>{for(c%floor(random([120,240,480]))==0&&(h=random(1,75)),o+=.02,i=0;i<W;i+=g)for(j=0;j<H;j+=g)d=dist(W/2,H/2,i+g/2,j+g/2),a=map(d,0,W/2,0,PI),s=abs(sin(sin(a)*h+o)),fill(.5<s?0:W),rect(i,j,g);c++}
+g=10,o=0,c=0,setup=o=>{createCanvas(W=windowWidth,H=windowHeight),noStroke()},draw=n=>{for(c%floor(random([120,240,480]))==0&&(h=random(1,75)),o+=.02,i=0;i<W;i+=g)for(j=0;j<H;j+=g)d=dist(W/2,H/2,i+g/2,j+g/2),a=map(d,0,W/2,0,PI),s=abs(sin(cos(a)*h+o/d+o/.2)),fill(.5<s?0:W),rect(i,j,g);c++}
